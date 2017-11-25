cd60ae28303617b215f79c594a840d5c462a7417
Koen Deschacht <koendeschacht@gmail.com>
2017-11-25 06:17:56 +0100

    Added utilities for micro analytics

---
+++ src/main/java/be/bagofwords/util/MicroAnalytics.java
@@ -0,0 +1,33 @@
+/*
+ * Created by Koen Deschacht (koendeschacht@gmail.com) 2017-11-25. For license
+ * information see the LICENSE file in the root folder of this repository.
+ */
+
+package be.bagofwords.util;
+
+import be.bagofwords.logging.Log;
+import be.bagofwords.web.BaseController;
+
+import java.util.Arrays;
+
+import static java.util.stream.Collectors.toList;
+
+public class MicroAnalytics {
+
+    public static void anal(Object... args) {
+        String message = String.join(" ", Arrays.stream(args).map(arg -> convertArgToString(arg)).collect(toList()));
+        Log.i("ANALYTICS " + message);
+    }
+
+    private static String convertArgToString(Object arg) {
+        if (arg instanceof String) {
+            return (String) arg;
+        } else if (arg instanceof BaseController) {
+            BaseController controller = (BaseController) arg;
+            return controller.getMethod() + " " + controller.getPath();
+        } else {
+            return SerializationUtils.serializeObject(arg);
+        }
+    }
+
+}
