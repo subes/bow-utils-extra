b4529b0432f2707804003643d08b959868410fc3
Koen Deschacht <koendeschacht@gmail.com>
2017-11-26 09:14:50 +0100

    Added analytics listeners

--- src/main/java/be/bagofwords/util/MicroAnalytics.java
+++ src/main/java/be/bagofwords/util/MicroAnalytics.java
@@ -8,15 +8,38 @@ package be.bagofwords.util;
 import be.bagofwords.logging.Log;
 import be.bagofwords.web.BaseController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
 
 import static java.util.stream.Collectors.toList;
 
 public class MicroAnalytics {
 
+    private static final List<Consumer<String>> listeners = new ArrayList<>();
+
     public static void track(Object... args) {
         String message = String.join(" ", Arrays.stream(args).map(arg -> convertArgToString(arg)).collect(toList()));
+        synchronized (listeners) {
+            Iterator<Consumer<String>> iterator = listeners.iterator();
+            while (iterator.hasNext()) {
+                try {
+                    iterator.next().accept(message);
+                } catch (Exception exp) {
+                    Log.w("Analytics listener threw an exception, will remove this listener", exp);
+                    iterator.remove();
+                }
+            }
+        }
         Log.i("ANALYTICS " + message);
+    }
+
+    public static void addAnalyticsListener(Consumer<String> listener) {
+        synchronized (listeners) {
+            listeners.add(listener);
+        }
     }
 
     private static String convertArgToString(Object arg) {
